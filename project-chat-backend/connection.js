229af4bbb7c9179fedcf976ff40e98b3f2e9c6a0
Travis Woods <woodstr3313@gmail.com>
2022-06-09 19:26:12 -0400

    multiple issues with pushing to previous git repo. Added new repo to push to

--- project-chat-backend/connection.js
+++ project-chat-backend/connection.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.9yjrr.mongodb.net/ProjectChat?retryWrites=true&w=majority`,()=>{
-    console.log('connected to mongodb')
+    console.log('connected t o mongodb')
 })
